6562b04882107b8e4cdc54bab4ee9963cec938ff
Ilya <ilyaandrs@yandex.ru>
2021-03-17 13:30:46 +0500

    Lesson № 2.8

---
+++ Chat/ChatCommon/MessageType.java
@@ -0,0 +1,11 @@
+package ru.geekbrains;
+
+public enum MessageType {
+    PUBLIC_MESSAGE,
+    PRIVATE_MESSAGE,
+    SEND_AUTH_MESSAGE,
+    AUTH_CONFIRM,
+    ERROR_MESSAGE,
+    ONLINE_STATUS
+
+}
